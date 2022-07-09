5598622f2f35a7e849df99c37aacc45d6013f0ea
Jorge Limas <novalys@gmail.com>
2022-07-08 21:02:05 -0600

    Create a setup function for LSP diagnostics

--- lua/lsp-config/init.lua
+++ lua/lsp-config/init.lua
@@ -3,6 +3,51 @@ if not status_ok then
   return
 end
 
+-- Function to setup the diagnostic LSP information.
+local function setupDiagnostics()
+  local signs = {
+
+    { name = 'DiagnosticSignError', text = '' },
+    { name = 'DiagnosticSignWarn', text = '' },
+    { name = 'DiagnosticSignHint', text = '' },
+    { name = 'DiagnosticSignInfo', text = '' },
+  }
+
+  for _, sign in ipairs(signs) do
+    vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = '' })
+  end
+
+  local config = {
+    virtual_text = false, -- disable virtual text
+    signs = {
+      active = signs, -- show signs
+    },
+    update_in_insert = true,
+    underline = true,
+    severity_sort = true,
+    float = {
+      focusable = true,
+      style = 'minimal',
+      border = 'rounded',
+      source = 'always',
+      header = '',
+      prefix = '',
+    },
+  }
+
+  vim.diagnostic.config(config)
+
+  vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, {
+    border = 'rounded',
+  })
+
+  vim.lsp.handlers['textDocument/signatureHelp'] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+    border = 'rounded',
+  })
+end
+
+setupDiagnostics()
+
 -- Install the LSP Servers
 lsp_installer.setup({
   ensure_installed = { 'tsserver', 'tailwindcss', 'pyright', 'html', 'sumneko_lua' },
@@ -16,34 +61,30 @@ lsp_installer.setup({
   }
 })
 
--- Mappings.
--- See `:help vim.diagnostic.*` for documentation on any of the below functions
-local opts = { noremap = true, silent = true }
-vim.keymap.set('n', ',e', vim.diagnostic.open_float, opts)
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, opts)
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, opts)
-vim.keymap.set('n', ',q', vim.diagnostic.setloclist, opts)
-
--- Use an on_attach function to only map the following keys
--- after the language server attaches to the current buffer
--- client, bufnr
+-- Bind function after LSP has been initialized.
 local on_attach = function(client, bufnr)
   require('notify')('Buffer attached to LSP Server: ' .. client.name, 'success', {
     title = 'LSP Information'
   })
 
   -- Mappings.
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
   local bufopts = { noremap = true, silent = true, buffer = bufnr }
+  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
   vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-  vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
   vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
+  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
+
+  -- Diagnostics Bindings
+  vim.keymap.set('n', 'gl', vim.diagnostic.open_float, bufopts)
+  vim.keymap.set('n', 'g]', vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set('n', 'g[', vim.diagnostic.goto_next, bufopts)
+  vim.keymap.set('n', 'ga', vim.diagnostic.setloclist, bufopts)
+
   vim.keymap.set('n', '<Space>k', vim.lsp.buf.signature_help, bufopts)
   vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
   vim.keymap.set('n', '<C-r>', vim.lsp.buf.rename, bufopts)
   vim.keymap.set('n', '<C-a>', vim.lsp.buf.code_action, bufopts)
-  vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
   vim.keymap.set('n', '<C-f>', vim.lsp.buf.formatting, bufopts)
 
   -- Remove formatting capabilities for some LSP servers
@@ -65,6 +106,7 @@ local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
 
 -- Formatters
+-- TODO: Break the null ls server into a separate plugin.
 local null_ls = require 'null-ls'
 local formatting = null_ls.builtins.formatting
 null_ls.setup({
