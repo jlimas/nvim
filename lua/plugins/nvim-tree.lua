c89396c61439bd00a40e6834e1a840f763269cfd
Jorge Limas <novalys@gmail.com>
2022-07-11 22:59:25 -0600

    Setup the nvim tree to filter files

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -3,6 +3,22 @@ if not nvim_tree_ok then
   return
 end
 
-nvim_tree.setup()
+nvim_tree.setup({
+  sort_by = 'case_sensitive',
+  view = {
+    adaptive_size = true,
+    mappings = {
+      list = {
+        { key = 'u', action = 'dir_up' },
+      },
+    },
+  },
+  renderer = {
+    group_empty = true,
+  },
+  filters = {
+    dotfiles = true,
+  }
+})
 
 vim.keymap.set('n', '<Leader>e', '<Cmd>NvimTreeToggle<CR>')
