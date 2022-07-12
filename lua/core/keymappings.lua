e94ec504ea00267e3f6002ed3511687d8753a2e3
Jorge Limas <novalys@gmail.com>
2022-07-11 23:26:50 -0600

    Refactor the keymappings

--- lua/core/keymappings.lua
+++ lua/core/keymappings.lua
@@ -1,21 +1,49 @@
+-- Shorten function name
+local keymap = vim.keymap.set
+
+-- Silent keymap options
+local opts = { silent = true }
+
+--Remap space as leader key
+keymap('', '<Space>', '<Nop>', opts)
 vim.g.mapleader = ' '
 
-vim.keymap.set('i', 'jk', '<esc>')
+-- Insert exit fast
+keymap('i', 'jk', '<esc>', opts)
 
-vim.keymap.set('n', '<leader>w', '<cmd>w<cr>')
-vim.keymap.set('n', '<leader>q', '<cmd>q<cr>')
-vim.keymap.set('n', '<leader>x', '<cmd>q!<cr>')
-vim.keymap.set('n', '<leader>c', '<cmd>bd<cr>')
+-- Clear highlights
+keymap('n', '<leader>h', '<cmd>nohlsearch<CR>', opts)
+
+-- Better paste
+keymap('v', 'p', '"_dP', opts)
+
+-- Window navigation
+keymap('n', '<leader>w', '<cmd>w<cr>', opts)
+keymap('n', '<leader>q', '<cmd>q<cr>', opts)
+keymap('n', '<leader>x', '<cmd>q!<cr>', opts)
+keymap('n', '<leader>c', '<cmd>bd<cr>', opts)
 
 -- Splits
-vim.keymap.set('n', '<S-Right>', '<cmd>vs<cr>')
-vim.keymap.set('n', '<S-Down>', '<cmd>sp<cr>')
+keymap('n', '<S-Right>', '<cmd>vs<cr>', opts)
+keymap('n', '<S-Down>', '<cmd>sp<cr>', opts)
 
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>')
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>')
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>')
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>')
+-- Resize with arrows
+keymap('n', '<C-h>', '<C-w><C-h>', opts)
+keymap('n', '<C-l>', '<C-w><C-l>', opts)
+keymap('n', '<C-j>', '<C-w><C-j>', opts)
+keymap('n', '<C-k>', '<C-w><C-k>', opts)
+
+-- Resize with arrows
+keymap('n', '<C-Up>', ':resize -2<CR>', opts)
+keymap('n', '<C-Down>', ':resize +2<CR>', opts)
+keymap('n', '<C-Left>', ':vertical resize -2<CR>', opts)
+keymap('n', '<C-Right>', ':vertical resize +2<CR>', opts)
 
 -- Easy Motion
-vim.keymap.set('n', 's', '<plug>(easymotion-s2)')
-vim.keymap.set('n', 't', '<plug>(easymotion-t2)')
+keymap('n', 's', '<plug>(easymotion-s2)', opts)
+keymap('n', 't', '<plug>(easymotion-t2)', opts)
+
+-- Visual --
+-- Stay in indent mode
+keymap('v', '<', '<gv', opts)
+keymap('v', '>', '>gv', opts)
